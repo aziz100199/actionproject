7c8e9368ddb3f279d537a63d5e5e60ce314ab029
Aziz <azizmalik0110@gmai.cm>
2022-03-14 12:24:16 +0500

    working fine

--- app/src/main/java/com/unit/spinneractivity/roomdatabase/fragments/ProfileFragment.kt
+++ app/src/main/java/com/unit/spinneractivity/roomdatabase/fragments/ProfileFragment.kt
@@ -36,7 +36,7 @@ import java.io.FileOutputStream
 class ProfileFragment : Fragment() {
 
     var binding: FragmentProfileBinding? = null
-    internal var imagePath: String? = null
+    internal var imagePath: String? = ""
     var dataentity: DataEntity? = null
     val requestcodeforgallery = 1
     val requestcondeforcamera = 2
@@ -89,8 +89,7 @@ class ProfileFragment : Fragment() {
             binding?.adressedittext?.setText(it.useraddress)
             if (it.imageuri != null)
                 binding?.insertimage?.setImageURI(Uri.parse(it.imageuri))
-            imagePath = it.imageuri
-
+            imagestring=it.imageuri
         }
     }
 
@@ -126,8 +125,7 @@ class ProfileFragment : Fragment() {
             val useremail = binding?.emailedittext?.text.toString()
             val userphone = binding?.phoneedittext?.text.toString()
             val useraddress = binding?.adressedittext?.text.toString()
-
-            imagePath?.let { it1 ->
+            imagestring?.let { it1 ->
                 dataentity?.username = username
                 dataentity?.useremail = useremail
                 dataentity?.imageuri = it1
